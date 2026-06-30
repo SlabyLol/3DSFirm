82ed172e2a7a3f5ccf2e418765cbe0ed29929322
DarkFox <darkfox.tobias@outlook.com>
2026-06-30 09:39:15 +0200

    Update main.c

--- arm11/source/main.c
+++ arm11/source/main.c
@@ -1,11 +1,6 @@
-#include <stdint.h>
-
-void __attribute__((noreturn)) main(void) {
-    // Der ARM11 wird vom ARM9 geweckt.
-    // Hier wird das Patching des originalen 3DS-Kernels (Firm Launch) vorbereitet.
-    
+int main(void) {
     while (1) {
-        // Deine Logik für Homebrew-Enabler, Debugger oder Overclocking
         __asm__("wfi");
     }
+    return 0;
 }
